3d97c3ebabe855a833aba7139effafe4a14ede46
Him10Agr <himanshuagr.c2@gmail.com>
2025-04-22 15:14:12 +0530

    Added verification method for transactions for block struct

--- lib/src/types.rs
+++ lib/src/types.rs
@@ -100,14 +100,19 @@ impl Block {
         return Hash::hash(self);
     }
 
-    pub fn verify_transaction(self: &Self, utxos: &HashMap<Hash, 
+    pub fn verify_transaction(self: &Self, 
+                            predicted_block_height: u64,
+                            utxos: &HashMap<Hash, 
                             TransactionsOutput>) -> Result<()> {
         let mut inputs: HashMap<Hash, TransactionsOutput> = HashMap::new();
         //reject empty blocks
         if self.transactions.is_empty() {
             return Err(BtcError::InvalidTransaction);
         }
-        for transaction in &self.transactions {
+
+        //verify coinbase transaction
+        self.verify_coinbase_transaction(predicted_block_height, utxos)?;
+        for transaction in self.transactions.iter().skip(1) {
             let mut input_value = 0;
             let mut output_value = 0;
             for input in &transaction.inputs {
@@ -142,6 +147,89 @@ impl Block {
         }
 
         return Ok(());
+    }
+
+    pub fn verify_coinbase_transaction(self: &Self, 
+        predicted_block_height: u64,
+        utxos: &HashMap<Hash, TransactionsOutput>
+    ) -> Result<()> {
+        //coinbase tx is the first tx in the block
+        let coinbase_transaction = &self.transactions[0];
+        if coinbase_transaction.inputs.len() != 0 {
+            return Err(BtcError::InvalidTransaction);
+        }
+        if coinbase_transaction.outputs.len() == 0 {
+            return Err(BtcError::InvalidTransaction);
+        }
+
+        //yet to implement function to calculate minor fees
+        let miner_fees = self.calculate_miner_fees(utxos)?;
+        let block_reward = crate::INITIAL_REWARD 
+                                * 10u64.pow(8)
+                                / 2u64.pow(
+                                    (predicted_block_height
+                                    / crate::HALVING_INTERVAL) as u32
+                                );
+        let total_coinbase_outputs: u64 = 
+                                coinbase_transaction
+                                .outputs
+                                .iter()
+                                .map(|output| output.value)
+                                .sum();
+        if total_coinbase_outputs != block_reward + miner_fees {
+            return Err(BtcError::InvalidTransaction);
+        }
+
+        return Ok(());
+    }
+
+    pub fn calculate_miner_fees(
+        self: &Self,
+        utxos: &HashMap<Hash, TransactionsOutput>
+    ) -> Result<u64> {
+        let mut inputs: HashMap<Hash, TransactionsOutput> = HashMap::new();
+        let mut outputs: HashMap<Hash, TransactionsOutput> = HashMap::new();
+        //check every transaction after coinbase
+        for transaction in self.transactions.iter().skip(1) {
+            for input in &transaction.inputs {
+                /*inputs do not contain
+                the values of the outputs 
+                so we need to match inputs
+                to outptuts*/
+                let prev_output = utxos.get(
+                    &input.prev_transaction_output_hash
+                );
+                if prev_output.is_none() {
+                    return Err(BtcError::InvalidTransaction);
+                }
+                let prev_output = prev_output.unwrap();
+                if inputs.contains_key(&input.prev_transaction_output_hash) {
+                    return Err(BtcError::InvalidTransaction);
+                }
+                inputs.insert(
+                    input.prev_transaction_output_hash,
+                    prev_output.clone()
+                );
+            }
+            for output in &transaction.outputs {
+                if outputs.contains_key(&output.hash()) {
+                    return Err(BtcError::InvalidTransaction);
+                }
+                outputs.insert(
+                    output.hash(),
+                    output.clone(),
+                );
+            }
+        }
+
+        let input_value: u64 = inputs.values()
+                                .map(|output| output.value)
+                                .sum();
+        let output_value: u64 = outputs.values()
+                                .map(|output| output.value)
+                                .sum();
+        
+        return Ok(input_value - output_value);
     }
 }
 
