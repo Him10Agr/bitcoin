f4d215c4d1c4c816dda0e3ddbc9c1622d15bac4d
Him10Agr <himanshuagr.c2@gmail.com>
2025-04-22 14:08:17 +0530

    Created constants for bitcoin lib

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -7,6 +7,22 @@ construct_uint!{
     pub struct U256(4);
 }
 
+//initial reward in bitcoin - x 10 ^ 8 to get satoshis
+pub const INITIAL_REWARD: u64  = 50;
+//halving interval in blocks
+pub const HALVING_INTERVAL: u64 = 210;
+//ideal block time in seconds
+pub const IDEAL_BLOCK_TIME: u64 = 10;
+//min target
+pub const MIN_TARGET: U256 = U256([
+    0xFFFF_FFFF_FFFF_FFFF,
+    0xFFFF_FFFF_FFFF_FFFF,
+    0xFFFF_FFFF_FFFF_FFFF,
+    0xFFFF_FFFF_FFFF_FFFF,
+]);
+//difficulty update interval in blocks
+pub const DIFFICULTY_UPDATE_INETRVAL: u64 = 50;
+
 pub mod sha256;
 pub mod types;
 pub mod util;
